8596ab0cd536b4d04bc17604f9916608de4ad78a
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-21 09:02:02 +0530

    Exercise 8: Iterate a given list and check if a given element exists as a key’s value in a dictionary. If not, delete it from the list

---
+++ Exercise 8.py
@@ -0,0 +1,9 @@
+# Iterate a given list and check if a given element exists as a key’s value in a dictionary. If not, delete it from the list
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+ans = []
+for i in roll_number:
+    if (i in sample_dict.values()):
+        ans.append(i)
+roll_number = ans
+print("After removing unwanted elements from list", roll_number)
