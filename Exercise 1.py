12f1138ad9990055b2177e5af391825007f5c69f
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-21 08:29:23 +0530

    Exercise 1: Create a list by picking an odd-index items from the first list and even index items from the second

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,6 +1,6 @@
 # Exercise 1: Create a list by picking an odd-index items from the first list and even index items from the second
-l1 = [int(i) for i in input().strip().split(",")]
-l2 = [int(i) for i in input().strip().split(",")]
+l1 = [3, 6, 9, 12, 15, 18, 21]
+l2 = [4, 8, 12, 16, 20, 24, 28]
 odd = []
 even = []
 ans = []
