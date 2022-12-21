4fe9319bdc58dd0ab2ca550bd420b30712649f88
Yash Kumar Gupta <ykgupta2411@gmail.com>
2022-12-21 08:45:46 +0530

    Exercise 5: Create a Python set such that it shows the element from both lists in a pair

---
+++ Exercise 5.py
@@ -0,0 +1,7 @@
+# Exercise 5: Create a Python set such that it shows the element from both lists in a pair
+first_list = [2, 3, 4, 5, 6, 7, 8]
+second_list = [4, 9, 16, 25, 36, 49, 64]
+ans = set()
+for i in range(len(first_list)):
+    ans.add((first_list[i], second_list[i]))
+print("Result is", ans)
